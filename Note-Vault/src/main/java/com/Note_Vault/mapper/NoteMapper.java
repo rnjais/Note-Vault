a02405c7bc0e771c47792c00ff0a7aa0ced77541
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-09-04 13:17:48 +0530

    NoteMapper

--- Note-Vault/src/main/java/com/Note_Vault/mapper/NoteMapper.java
+++ Note-Vault/src/main/java/com/Note_Vault/mapper/NoteMapper.java
@@ -1,4 +1,34 @@
 package com.Note_Vault.mapper;
 
+import com.Note_Vault.dto.NoteDTO;
+import com.Note_Vault.entity.Note;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NoteMapper {
+
+    // Entity → DTO
+    public static NoteDTO toDTO(Note note) {
+
+        NoteDTO dto = new NoteDTO();
+
+        dto.setId(note.getId());
+        dto.setTitle(note.getTitle());
+        dto.setContent(note.getContent());
+        dto.setCategory(note.getCategory());
+
+        return dto;
+    }
+
+    // DTO → Entity
+    public static Note toEntity(NoteDTO dto) {
+
+        Note note = new Note();
+
+        note.setTitle(dto.getTitle());
+        note.setContent(dto.getContent());
+        note.setCategory(dto.getCategory());
+
+        return note;
+    }
 }
